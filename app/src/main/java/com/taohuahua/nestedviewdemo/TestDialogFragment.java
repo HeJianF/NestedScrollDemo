d3806f0627c2731cf7e9da28a0b9fcb36bf0c098
BJQXDN0811 <hejianfeng@36kr.com>
2020-02-10 18:36:59 +0800

    升级androidx

--- app/src/main/java/com/taohuahua/nestedviewdemo/TestDialogFragment.java
+++ app/src/main/java/com/taohuahua/nestedviewdemo/TestDialogFragment.java
@@ -2,12 +2,12 @@ package com.taohuahua.nestedviewdemo;
 
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/taohuahua/nestedviewdemo/scroll/TopToBottomFinishLayout.java
+++ app/src/main/java/com/taohuahua/nestedviewdemo/scroll/TopToBottomFinishLayout.java
@@ -1,10 +1,10 @@
 package com.taohuahua.nestedviewdemo.scroll;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.view.NestedScrollingParent2;
-import android.support.v4.view.ViewCompat;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.NestedScrollingParent2;
+import androidx.core.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
